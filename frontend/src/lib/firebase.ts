1d469f9dcd4e7c02d1c3172266ccc018910c8f04
Abhijit Bhunia <abhijitbhunia@virtualemployee.com>
2026-06-25 15:23:06 +0530

    firebase fixed

--- frontend/src/lib/firebase.ts
+++ frontend/src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw8JRPRAEoy2jnNOEBB6pbC_pUOj7q_8w",
-  authDomain: "atlas-3e486.firebaseapp.com",
-  projectId: "atlas-3e486",
-  storageBucket: "atlas-3e486.firebasestorage.app",
-  messagingSenderId: "697128380143",
-  appId: "1:697128380143:web:b6cbc417fc14e14f979d1a",
-  measurementId: "G-1G6P6WF7W3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
